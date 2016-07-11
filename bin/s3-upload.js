1f6e5a30a8437a68bf09e2b8e73511c8459fc991
MishaHerscu <mishaherscu@gmail.com>
2016-07-11 09:37:17 -0400

    Save upload information in datastore
    Use upload model and mongoose middleware, be sure to close connection.
    Storing in the datastore should happen only after a successful S3 response.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,9 +1,15 @@
 'use strict';
 
+
 const fs = require('fs');
+
+const mongoose = require('../app/middleware/mongoose.js');
+const Upload = require('../app/models/upload.js');
+
 const uploader = require('../lib/aws-s3-upload.js');
 
 let filename = process.argv[2] || '';
+let comment = process.argv[3] || 'no comment';
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -20,5 +26,15 @@ const readFile = (filename) => {
 readFile(filename)
 .then(uploader.prepareFile)
 .then(uploader.awsUpload)
+.then((response) => {
+  let upload = {
+    location: response.Location,
+    comment: comment, // not defined yet
+  };
+
+  return Upload.create(upload);
+
+})
 .then(console.log)
-.catch(console.error);
+.catch(console.error)
+.then(() => mongoose.connection.close());
